bb0ab6bc7fa68d8d4775ebfff6542db167ff40e1
thomasyu21 <>
2021-01-04 21:14:15 -0500

    Wrote method pigLatinBest as well as the main method to take a file. Fixed the previous 2 methods to make the string lowercase in the beginning rather than the end.

--- PigLatin.java
+++ PigLatin.java
@@ -1,23 +1,45 @@
 import java.util.*;
 public class PigLatin{
   public static void main( String[]args ){
+      Scanner n = new Scanner( System.in );
 
+      while (n.hasNextLine()){
+        Scanner m = new Scanner(n.nextLine());
+        while (m.hasNext()){
+          System.out.print(pigLatinBest(m.next()));
+          if (m.hasNext()){
+            System.out.print(" ");
+          }
+        }
+        System.out.println();
+      }
     /*
     System.out.println(pigLatinSimple("mock"));
     System.out.println(pigLatinSimple("pie"));
-    System.out.println(pigLatinSimple("david"));
-    System.out.println(pigLatinSimple("aaron"));
+    System.out.println(pigLatinSimple("David"));
+    System.out.println(pigLatinSimple("Aaron"));
     System.out.println();
 
     System.out.println(pigLatin("the"));
     System.out.println(pigLatin("check"));
-    System.out.println(pigLatin("skee"));
+    System.out.println(pigLatin("Skee"));
     System.out.println(pigLatin("emu"));
-    System.out.println(pigLatin("grade"));
+    System.out.println(pigLatin("Grade"));
+    System.out.println();
+
+    System.out.println(pigLatinBest("*emu"));
+    System.out.println(pigLatinBest("4chan"));
+    System.out.println(pigLatinBest("fish!"));
+    System.out.println(pigLatinBest("fish"));
+    System.out.println(pigLatinBest("the."));
+    System.out.println(pigLatinBest("Cat!"));
+    System.out.println(pigLatinBest("Amazing?"));
+    System.out.println(pigLatinBest("Apple%"));
     */
   }
 
   public static String pigLatinSimple(String s){
+    s = s.toLowerCase();
     String[] vowels = {"a","e","i","o","u"};
     String result = "";
     boolean vowelStart = false;
@@ -35,6 +57,7 @@ public class PigLatin{
   }
 
   public static String pigLatin(String s){
+    s = s.toLowerCase();
     String[] digraphs = {"bl", "br", "ch", "ck", "cl", "cr", "dr", "fl", "fr", "gh", "gl", "gr", "ng", "ph", "pl", "pr", "qu", "sc", "sh", "sk", "sl", "sm", "sn", "sp", "st", "sw", "th", "tr", "tw", "wh", "wr"};
     boolean digraphStart = false;
     if (s.length() > 2){
@@ -50,6 +73,32 @@ public class PigLatin{
     }else{
       result = s;
     }
-    return (pigLatinSimple(result));
+    return pigLatinSimple(result);
+  }
+
+  public static String pigLatinBest(String s){
+    s = s.toLowerCase();
+    String[] notPunctuation = {"a", "b", "c", "d", "e", "f", "g","h", "i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z","0","1","2","3","4","5","6","7","8","9"};
+    boolean startLetter = false;
+    boolean endPunctuation = true;
+    for (int i = 0; i < 26; i++){
+      if (s.substring(0, 1).equals(notPunctuation[i])){
+        startLetter = true;
+      }
+    }
+    for (int i = 0; i < notPunctuation.length; i++){
+      if (s.substring(s.length()-1, s.length()).equals(notPunctuation[i])){
+        endPunctuation = false;
+      }
+    }
+    if (startLetter){
+      if (endPunctuation){
+        return pigLatin(s.substring(0, s.length()-1)) + s.substring(s.length()-1, s.length());
+      }else{
+        return pigLatin(s);
+      }
+    }else{
+      return s;
+    }
   }
 }
